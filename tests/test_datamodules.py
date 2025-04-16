bee6347a4ce45cf19b0cc7a6c8d8c39c233cf66e
Guillaume <guillaume.sabatier57@gmail.com>
2025-04-16 16:23:30 +0200

    4eme commit, mdif du readme (partielement)+premiere modif du test_datamodule

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -5,7 +5,6 @@ import torch
 
 from src.data.mnist_datamodule import MNISTDataModule
 
-#checke si c'est vraiment util, tout depend de ce que eux font (si il utilise mnist ou non)
 @pytest.mark.parametrize("batch_size", [32, 128])
 def test_mnist_datamodule(batch_size: int) -> None:
     """Tests `MNISTDataModule` to verify that it can be downloaded correctly, that the necessary
@@ -19,16 +18,9 @@ def test_mnist_datamodule(batch_size: int) -> None:
     dm = MNISTDataModule(data_dir=data_dir, batch_size=batch_size)
     dm.prepare_data()
 
-    assert not dm.data_train and not dm.data_val and not dm.data_test
-    assert Path(data_dir, "MNIST").exists()
-    assert Path(data_dir, "MNIST", "raw").exists()
-
     dm.setup()
     assert dm.data_train and dm.data_val and dm.data_test
     assert dm.train_dataloader() and dm.val_dataloader() and dm.test_dataloader()
-
-    num_datapoints = len(dm.data_train) + len(dm.data_val) + len(dm.data_test)
-    assert num_datapoints == 70_000
 
     batch = next(iter(dm.train_dataloader()))
     x, y = batch
